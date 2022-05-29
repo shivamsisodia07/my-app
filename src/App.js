522bc46c4952dee6b6b1840a08b999ae5e1466ab
shivam <shivamsisodia8656816@gmail.com>
2022-05-29 18:11:44 +0530

    changes in menucomponet

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
       <div className='App'>
         <Navbar dark color='primary'>
           <div className='container'>
-            <NavbarBrand href='/'>reactstrap</NavbarBrand>
+            <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
         <Menu dishes={this.state.dishes} />
